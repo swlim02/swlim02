3e7d967b4fa8c6059d62d1baf3ef0df4a5111e2a
swlim02 <foryourclub@gmail.com>
2021-12-01 11:54:58 +0900

    add TODO @ghjeong

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,20 +1,28 @@
-import React, { Component, Fragment, useState  } from 'react';
+import React, { Component, Fragment, useState, useRef  } from 'react';
 import Select from 'react-select'
+import * as d3 from "d3";
 
 const ControlPanel = (props) => {
+  const [severitylevel, setSeveritylevel] = useState(1); // 기본값 1단계
+  const controlSvg = useRef(null);
+  // TODO @ghjeong
+  // 디폴트 레벨로 레벨바 이미지 그리기
+  // 사각형, 라운드사각형 이용
 
   let options_x = []
 
-  options_x.push({"value":'1 단계', "label":'1 단계'});
-  options_x.push({"value":'2 단계', "label":'2 단계'});
-  options_x.push({"value":'3 단계', "label":'3 단계'});
-  options_x.push({"value":'4 단계', "label":'4 단계'});
+  options_x.push({"value":1, "label":'1 단계'});
+  options_x.push({"value":2, "label":'2 단계'});
+  options_x.push({"value":3, "label":'3 단계'});
+  options_x.push({"value":4, "label":'4 단계'});
 
   function handleChange_x(evt) {
     props.selectedOptions.selectedOption_x = evt.value;
 //    props.update_f();
+// TODO @ghjeong
+// 레벨 변경시 이미지 변경되도록 작성
+// state 변경시 렌더링다시되는 것을 이용하면 됨 "setSeveritylevel(n)" 호출
   }
-
 
   return (
     <div class="selectionPanel">
