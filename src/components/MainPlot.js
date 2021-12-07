fb4f10e7bece7109564f3dfb1584be5fb10e554f
swlim02 <foryourclub@gmail.com>
2021-12-07 20:01:00 +0900

    getDayTrendOfBuildingFloorDensity,getDayTrendOfBuildingRoomDensity개발

--- src/components/MainPlot.js
+++ src/components/MainPlot.js
@@ -221,8 +221,6 @@ const MainPlot = (props) => {
     let selected_date = bdNumber+"_w_"+startDate;
     console.log("dayBuildingSelected_date:"+selected_date);
     selected_date = "301_w_2021-11-22"; // 개발위해 임시 TODO 삭제
-    console.log("CrowdDensityTrendInfo[selected_date][0].capacity");
-    console.log(CrowdDensityTrendInfo[selected_date][0].capacity);
     if (CrowdDensityTrendInfo[selected_date] == null) return null;
     DayTrend = {
      "capacity" : CrowdDensityTrendInfo[selected_date][0].capacity,
@@ -234,26 +232,38 @@ const MainPlot = (props) => {
   }
 
   function getDayTrendOfBuildingFloorDensity(bdNumber, floor, startDate,endDate) {
+    if (startDate === null || bdNumber === null || floor === null) return null;
+
     let DayTrend = new Object();
+    let selected_date = bdNumber+"_"+floor+"_w_"+startDate;
+    console.log("DayFloorDensity");
+    console.log(selected_date);
+    selected_date = "301_1_w_2021-11-22"; // 개발위해 임시 TODO 삭제
+    if (CrowdDensityTrendInfo[selected_date] == null) return null;
     DayTrend = {
-     "capacity" : 300000,
-     "reserve_occupancy_trend" : [ //per day
-      20000, 30000, 10000, 20000, 30000 ],
-     "bq_occupancy_trend" : [ //per day
-      20000, 30000, 10000, 20000, 30000 ]
+     "capacity" : CrowdDensityTrendInfo[selected_date][0].capacity,
+     "reserve_occupancy_trend" : CrowdDensityTrendInfo[selected_date][0].reserve_occupancy_trend,
+     "bq_occupancy_trend" : CrowdDensityTrendInfo[selected_date][0].bq_occupancy_trend
     }
+    console.log(DayTrend);
     return DayTrend;
   }
 
   function getDayTrendOfBuildingRoomDensity(bdNumber, floor, roomNumber, startDate,endDate) {
+    if (startDate === null || bdNumber === null || floor === null || roomNumber === null) return null;
+
     let DayTrend = new Object();
+    let selected_date = bdNumber+"_"+floor+"_"+roomNumber+"_w_"+startDate;
+    console.log("DayRoomDensity");
+    console.log(selected_date);
+    selected_date = "301_1_118_w_2021-11-22"; // 개발위해 임시 TODO 삭제
+    if (CrowdDensityTrendInfo[selected_date] == null) return null;
     DayTrend = {
-     "capacity" : 300000,
-     "reserve_occupancy_trend" : [ //per day
-      20000, 30000, 10000, 20000, 30000 ],
-     "bq_occupancy_trend" : [ //per day
-      20000, 30000, 10000, 20000, 30000 ]
+     "capacity" : CrowdDensityTrendInfo[selected_date][0].capacity,
+     "reserve_occupancy_trend" : CrowdDensityTrendInfo[selected_date][0].reserve_occupancy_trend,
+     "bq_occupancy_trend" : CrowdDensityTrendInfo[selected_date][0].bq_occupancy_trend
     }
+    console.log(DayTrend);
     return DayTrend;
   }
 
